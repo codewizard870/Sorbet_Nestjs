c49ca49096c0298ce68218d31bdf4305a69f4e93
daenamcclintock <daena.mcclintock@gmail.com>
2023-03-20 15:53:59 +0700

    bug fix, userId

--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -17,7 +17,7 @@ export class ImagesService {
     console.log('bucketName', bucketName)
     console.log('userId', userId)
     const bucket = this.storage.bucket(bucketName)
-    console.log('storage', this.storage)
+    // console.log('storage', this.storage)
     console.log('bucket', bucket)
     const gcsFileName = `${userId}.png`
     const options = {
