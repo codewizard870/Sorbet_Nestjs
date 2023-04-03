32b45e2594be21c52432f112aa0c54427b7afdb5
daenamcclintock <daena.mcclintock@gmail.com>
2023-04-03 14:53:06 +0800

    added createSpotifyAccessToken endpoint for oauth integration for Spotify widget

--- src/models/widgets/widgets.module.ts
+++ src/models/widgets/widgets.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { WidgetsService } from "./widgets.service";
 import { WidgetsController } from "./widgets.controller";
 import { PrismaService } from "src/utils/prisma/prisma.service";
+import { HttpService } from '@nestjs/axios';
 
 @Module({
   controllers: [WidgetsController],
